13dcb1c0d43c02349acd7733f3ed3ddcebcd08ed
prakhartkg <prakhar.jain@YITRNG17DT.Yash.com>
2016-01-07 18:15:12 +0530

    asd

---
+++ cdix/cdix/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.yash.cdix;
+
+import java.util.List;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+@Default
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+	@Inject
+	private EmployeeDao employeeDao;
+	public EmployeeDao getEmployeeDao() {
+		return employeeDao;
+	}
+
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+	@Override
+	public List getAllEmployee() {
+		
+		return employeeDao.getAllEmployee();
+	}
+
+}
